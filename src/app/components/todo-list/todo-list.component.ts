696e52ceb27fe9516371db7ccc7aa5e2d3d13ef6
omar <omasrt@gmail.com>
2019-07-26 15:14:55 -0500

    adds checkall method and remaining todos

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -11,9 +11,12 @@ export class TodoListComponent implements OnInit {
   todoTitle: string;
   idForTodo: number;
   beforeEditCache: string;
+  filter: string;
+
   constructor() {}
 
   ngOnInit() {
+    this.filter = "all";
     this.beforeEditCache = "";
     this.idForTodo = 4;
     this.todoTitle = "";
@@ -76,5 +79,29 @@ export class TodoListComponent implements OnInit {
 
   remaining(): number {
     return this.todos.filter(todo => !todo.completed).length;
+  }
+
+  atLeastOneCompleted(): boolean {
+    return this.todos.filter(todo => todo.completed).length > 0;
+  }
+
+  clearCompleted(): void {
+    this.todos = this.todos.filter(todo => !todo.completed);
+  }
+
+  checkAllTodos(): void {
+    this.todos.forEach(
+      todo => (todo.completed = (<HTMLInputElement>event.target).checked)
+    );
+  }
+
+  todosFiltered(): Todo[] {
+    if (this.filter === "all") {
+      return this.todos;
+    } else if (this.filter === "active") {
+      return this.todos.filter(todo => !todo.completed);
+    } else if (this.filter === "completed") {
+      return this.todos.filter(todo => todo.completed);
+    }
   }
 }
