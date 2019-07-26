81628656c136471213c6dd37f78ff7cd9a6f38ce
omar <omasrt@gmail.com>
2019-07-26 13:28:07 -0500

    sets state and add todo methods

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -6,7 +6,48 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./todo-list.component.scss"]
 })
 export class TodoListComponent implements OnInit {
+  todos: object[];
+  todoTitle: string;
+  idForTodo: number;
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.idForTodo = 4;
+    this.todoTitle = "";
+    this.todos = [
+      {
+        id: 1,
+        title: "Finish Angular Screencast",
+        completed: false,
+        editing: false
+      },
+      {
+        id: 2,
+        title: "Take over world",
+        completed: false,
+        editing: false
+      },
+      {
+        id: 3,
+        title: "One more thing",
+        completed: false,
+        editing: false
+      }
+    ];
+  }
+
+  addTodo(): void {
+    if (this.todoTitle.trim().length === 0) {
+      return;
+    }
+    this.todos.push({
+      id: this.idForTodo,
+      title: this.todoTitle,
+      completed: false,
+      editing: false
+    });
+
+    this.todoTitle = "";
+    this.idForTodo++;
+  }
 }
